57c3ef2c1662a15c44b4a2ea2e3f304ee5b7d302
Zo <197201481+zooffbyte@users.noreply.github.com>
2025-02-01 16:08:00 +0700

    Add basic UT coverage scenarios

--- src/Betsson.OnlineWallets.UnitTests/OnlineWalletServiceTests.cs
+++ src/Betsson.OnlineWallets.UnitTests/OnlineWalletServiceTests.cs
@@ -2,6 +2,7 @@ using Betsson.OnlineWallets.Data.Repositories;
 using Betsson.OnlineWallets.Data.Models;
 using Betsson.OnlineWallets.Models;
 using Betsson.OnlineWallets.Services;
+using Betsson.OnlineWallets.Exceptions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System.Threading.Tasks;
@@ -43,25 +44,200 @@ namespace Betsson.OnlineWallets.UnitTests
 
     public class OnlineWalletServiceTests
     {
-        private readonly IOnlineWalletService _wallet;
+        // private readonly IOnlineWalletService _wallet;
 
-        public OnlineWalletServiceTests()
+        // public OnlineWalletServiceTests()
+        // {
+        //     var options = new DbContextOptionsBuilder<OnlineWalletContextMock>()
+        //         .UseInMemoryDatabase(databaseName: "TestDbContext")
+        //         .Options;
+        //     var context = new OnlineWalletContextMock(options);
+        //     var repository = new OnlineWalletRepositoryMock(context);
+
+        //     this._wallet = new OnlineWalletService(repository);
+        // }
+
+        public IOnlineWalletService WalletFixture()
         {
             var options = new DbContextOptionsBuilder<OnlineWalletContextMock>()
-                .UseInMemoryDatabase(databaseName: "TestDbContext")
+                .UseInMemoryDatabase(databaseName: "TestDbContext_" + System.Guid.NewGuid())
                 .Options;
             var context = new OnlineWalletContextMock(options);
             var repository = new OnlineWalletRepositoryMock(context);
 
-            _wallet = new OnlineWalletService(repository);
+            return new OnlineWalletService(repository);
         }
 
         [Fact]
         public async Task CustomerOpenBalance_ShouldBeZero()
         {
-            Balance currentBalance = await _wallet.GetBalanceAsync();
+            // Arrange
+            IOnlineWalletService wallet = WalletFixture();
 
+            // Act
+            Balance currentBalance = await wallet.GetBalanceAsync();
+
+            // Assert
             Assert.Equal(0, currentBalance.Amount);
+        }
+
+        [Fact]
+        public async Task CustomerDepositAmount_ShouldBeVisibleOnAccount()
+        {
+            // Arrange
+            IOnlineWalletService wallet = WalletFixture();
+            Deposit deposit = new Deposit();
+            deposit.Amount = 3;
+
+            // Act
+            Balance newBalance = await wallet.DepositFundsAsync(deposit);
+
+            // Assert
+            Assert.Equal(deposit.Amount, newBalance.Amount);
+        }
+
+        [Fact]
+        public async Task CustomerDepositMultipleAmount_ShouldBeVisibleOnAccount()
+        {
+            // Arrange
+            IOnlineWalletService wallet = WalletFixture();
+            int[] amounts = {3, 4, 7};
+            int expAmount = 0;
+            Deposit deposit = new Deposit();
+
+            for (int i = 0; i < amounts.Length; i++)
+            {
+                deposit.Amount = amounts[i];
+                expAmount += amounts[i];
+
+                // Act
+                Balance newBalance = await wallet.DepositFundsAsync(deposit);
+
+                // Assert
+                Assert.Equal(expAmount, newBalance.Amount);
+            }
+        }
+
+        [Fact]
+        public async Task CustomerWithdrawAmount_ShouldBeVisibleOnAccount()
+        {
+            // Arrange
+            IOnlineWalletService wallet = WalletFixture();
+            Deposit deposit = new Deposit();
+            deposit.Amount = 30;
+            Withdrawal withdrawal = new Withdrawal();
+            withdrawal.Amount = 7;
+
+            _ = await wallet.DepositFundsAsync(deposit);
+
+            // Act
+            Balance newBalance = await wallet.WithdrawFundsAsync(withdrawal);
+
+            // Assert
+            Assert.Equal(deposit.Amount - withdrawal.Amount, newBalance.Amount);
+        }
+
+        [Fact]
+        public async Task CustomerWithdrawMultipleAmount_ShouldBeVisibleOnAccount()
+        {
+            // Arrange
+            IOnlineWalletService wallet = WalletFixture();
+            int initialAmount = 40;
+            int[] amounts = {3, 4, 7};
+            int expAmount = initialAmount;
+            Deposit deposit = new Deposit();
+            deposit.Amount = initialAmount;
+            Withdrawal withdrawal = new Withdrawal();
+            withdrawal.Amount = 7;
+
+            _ = await wallet.DepositFundsAsync(deposit);
+
+            for (int i = 0; i < amounts.Length; i++)
+            {
+                withdrawal.Amount = amounts[i];
+                expAmount -= amounts[i];
+
+                // Act
+                Balance newBalance = await wallet.WithdrawFundsAsync(withdrawal);
+
+                // Assert
+                Assert.Equal(expAmount, newBalance.Amount);
+            }
+        }
+
+        [Fact]
+        public async Task CustomerReadBalanceAfterMultipleAmount_ShouldShowProperAmountOnAccount()
+        {
+            // Arrange
+            IOnlineWalletService wallet = WalletFixture();
+            Deposit deposit = new Deposit();
+            deposit.Amount = 60;
+            Withdrawal withdrawal = new Withdrawal();
+            withdrawal.Amount = 33;
+
+            // Act
+            _ = await wallet.DepositFundsAsync(deposit);
+            Balance currentBalanceAfterFirstDeposit = await wallet.GetBalanceAsync();
+
+            // Assert
+            Assert.Equal(deposit.Amount, currentBalanceAfterFirstDeposit.Amount);
+
+            // Act
+            _ = await wallet.WithdrawFundsAsync(withdrawal);
+            Balance currentBalanceAfterFirstWithdrawal = await wallet.GetBalanceAsync();
+
+            // Assert
+            Assert.Equal(deposit.Amount - withdrawal.Amount, currentBalanceAfterFirstWithdrawal.Amount);
+        }
+
+        [Fact]
+        public async Task BankSystemDepositMinusAmount_ShouldResultInError()
+        {
+            // Arrange
+            IOnlineWalletService wallet = WalletFixture();
+            Deposit deposit = new Deposit();
+            deposit.Amount = -10;
+
+            // Act
+            Balance newBalance = await wallet.DepositFundsAsync(deposit);
+
+            // Assert
+            Assert.Equal(0, newBalance.Amount);
+        }
+
+        // [Fact]
+        // public async Task BankSystemDepositMoreThanDecimalMaxAmount_ShouldResultInError()
+        // {
+        //     // Arrange
+        //     IOnlineWalletService wallet = WalletFixture();
+        //     Deposit deposit = new Deposit();
+        //     // deposit.Amount = Decimal.MaxValue + Decimal(1);
+        //     deposit.Amount = 79228162514264337593543950336;
+
+        //     // Act
+        //     Balance newBalance = await wallet.DepositFundsAsync(deposit);
+
+        //     // Assert
+        //     Assert.Equal(0, newBalance.Amount);
+        // }
+
+        [Fact]
+        public async Task CustomerTryToWithdrawMinusAmount_ShouldResultInError()
+        {
+            // Arrange
+            IOnlineWalletService wallet = WalletFixture();
+            Deposit deposit = new Deposit();
+            deposit.Amount = 10;
+            Withdrawal withdrawal = new Withdrawal();
+            withdrawal.Amount = -3;
+
+            _ = await wallet.DepositFundsAsync(deposit);
+
+            // Act
+            Balance newBalance = await wallet.WithdrawFundsAsync(withdrawal);
+
+            // Assert
+            Assert.Equal(deposit.Amount, newBalance.Amount);
         }
     }
 }
